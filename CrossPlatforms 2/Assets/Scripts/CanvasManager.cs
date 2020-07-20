d3cbda46777478df6825bc6a2172a9c0e3e88628
varant-t <vv_titizian@hotmail.ca>
2020-07-20 14:41:53 -0400

    CP -LAB 1

---
+++ CrossPlatforms 2/Assets/Scripts/CanvasManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour
+{
+    GameManager gm;
+
+    public Button startButton;
+    public Button quitButton;
+    public Button returnButton;
+
+
+    public GameObject pauseMenu;
+    AudioSource pauseAudio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseAudio = GetComponent<AudioSource>();
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+        if (startButton)
+        {
+            startButton.onClick.AddListener(gm.StartGame);
+        }
+
+        if (quitButton)
+        {
+            quitButton.onClick.AddListener(gm.QuitGame);
+        }
+
+        if (returnButton)
+        {
+            returnButton.onClick.AddListener(gm.Return);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            pauseMenu.SetActive(!pauseMenu.activeSelf);
+            if (pauseMenu.activeSelf)
+            {
+                pauseAudio.Play();
+            }
+        }
+        if (pauseMenu)
+        {
+            if (pauseMenu.activeSelf)
+            {
+                Time.timeScale = 0.0f;
+
+            }
+            else
+            {
+                Time.timeScale = 1.0f;
+            }
+        }
+    }
+
+}
---
+++ CrossPlatforms 2/Assets/Scripts/GameObjective.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class GameObjective : MonoBehaviour
+{
+    GameManager gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("GameOver");
+         
+        }
+    }
+
+}
